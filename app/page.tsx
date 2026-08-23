94ccb8a2a4587cede366e86b9afdced862452ba9
Peace Onyenweze <onyenwezechinwendupeace@gmail.com>
2026-08-23 19:08:30 +0100

    Added the featured property

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Showcase from "@/component/home/showcase";
 import Showsection from '@/component/home/show';
 import SalesSection from '@/component/home/sales';
 import HowItWorks from '@/component/home/howitworks';
+import FeatureProperties from '@/component/home/features';
 
 const page = () => {
   return (
@@ -12,6 +13,7 @@ const page = () => {
       <Showsection />
       <SalesSection />
       <HowItWorks />
+      <FeatureProperties />
     </div>
   )
 }
