6ab4e4b046cc107a638b423f5ce933a5489a965a
Peace Onyenweze <onyenwezechinwendupeace@gmail.com>
2026-08-24 15:02:10 +0100

    make little changes

---
+++ component/footer.tsx
@@ -0,0 +1,163 @@
+// import React from 'react'
+// import Link from 'next/link'
+
+// const Footer = () => {
+//   return (
+//     <section className="bg-[#1E5A4FB2] p-8">
+//         {/* flex container */}
+//         <div className="flex">
+//             {/* 1st flex item */}
+//             <div className="flex flex-col w-1/5">
+//                 <div className="flex">
+//                     <img src="./SH-LOGO.svg" alt="SH-LOGO" className="w-[73px] h-[90px]" />
+//                     <h1 className="font-bold text-2xl pt-9">
+//                     <span className="text-[#ffffff]">SPATIAL</span>
+//                     <span className="text-[#1E5A4F]">HUNT</span>
+//                      </h1>
+//                 </div>
+//                 <p className="text-[#ffffff]">Verified, Direct-to-Landlord, Property Marketplace.</p>
+//                 <p className="text-[#ffffff]">Built for a safer & smarter way to rent properties in Nigeria.</p>
+//                 {/* social icons */}
+//                 <div className="flex gap-3 pt-2">
+//                     <a href="">
+//                         <img src="./fbicon.svg" alt="fb-icon" className="h-6 w-6" />
+//                     </a>
+//                     <a href="https://www.instagram.com/thespatialhunt?utm_source=qr&igsh=MWpiNGM3em1kbXoweg%3D%3D">
+//                         <img src="./instagramicon.svg" alt="instagram-icon" className="h-6 w-6" />
+//                     </a>
+//                     <a href="https://www.tiktok.com/@spatialhunt">
+//                         <img src="./tiktok.svg" alt="linkedin-icon" className="h-6 w-6" />
+//                     </a>
+//                     <a href="https://x.com/thespatialhunt">
+//                         <img src="./x.svg" alt="youtube-icon" className="h-6 w-6" />
+//                     </a>
+//                 </div>
+//             </div>
+//             {/* 2nd flex */}
+//             <div className="flex flex-col w-1/5 text-[#ffffff] ml-15 mt-3">
+//             <h1 className="font-bold pb-2">QUICK LINKS</h1>
+//             {/* page links */}
+//             <ul>
+//                 <li>
+//             <Link href="/properties" className="font-medium pt-2">
+//               Properties
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/" className="font-medium pt-1">
+//               How it works
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/about" className="font-medium pt-1">
+//               About us
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/resources" className="font-medium pt-1">
+//               Resources
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/contact" className="font-medium pt-1">
+//               Contact us
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link
+//               href="/"
+//               className="font-bold pt-1">
+//               DOWNLOAD OUR APP
+//             </Link>
+//           </li>
+
+//             </ul>
+
+//             </div>
+
+//         {/* 3rd flex item */}
+//         <div className="flex flex-col w-1/5 text-[#ffffff] ml-12 mt-3">
+//         <h1 className="font-bold pb-2">FOR LANDLORDS</h1>
+//         {/* land lordlinks */}
+//         <ul>
+//          <li>
+//             <Link href="/listproperty" className="font-medium pt-2">
+//               List Your Property
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/listproperty" className="font-medium pt-1">
+//               Landlords Benefits
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/about" className="font-medium pt-1">
+//               About us
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/pricing" className="font-medium pt-1">
+//               Pricing
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/" className="font-medium pt-1">
+//               Success Stories
+//             </Link>
+//         </li>
+//         </ul>
+        
+//         </div>
+
+//         {/* 3rd flex item */}
+//         <div className="flex flex-col w-1/5 text-[#ffffff] ml-12 mt-3">
+//         <h1 className="font-bold pb-2">FOR LANDLORDS</h1>
+//         {/* land lordlinks */}
+//         <ul>
+//          <li>
+//             <Link href="/listproperty" className="font-medium pt-2">
+//               List Your Property
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/listproperty" className="font-medium pt-1">
+//               Landlords Benefits
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/about" className="font-medium pt-1">
+//               About us
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/pricing" className="font-medium pt-1">
+//               Pricing
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link href="/" className="font-medium pt-1">
+//               Success Stories
+//             </Link>
+//         </li>
+//         </ul>
+        
+//         </div>
+//         </div>
+//     </section>
+//   )
+// }
+
+// export default Footer
